ff0e8c8fcfab30e82374663933313aca2528edc5
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2022-08-25 18:13:20 +1000

    Updated README.md and added some comments

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 - Safe, reliable usage
 - Tiny binary size
 - Minimal dependences
+
+## Usage
+Currently, racklight does not have any Udev rules set, so the only way to set the brightness is to run it as root.
+This will be fixed at some point in the future, however for the meantime you can use something like sudo or doas to run the binary.
+
+```
+sudo -E cargo run acpi_video0 30
+```
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use racklight::backlight::Backlight;
 use std::env;
 
 fn main() {
+    // Collect arguments
     let mut args = env::args();
-
     let backlight = args.nth(1).unwrap();
     let value = args.next().unwrap();
 
+    // Create a new backlight with the given path
     let mut backlight = Backlight::new(backlight);
 
+    // Set the backlight brightness to the given value
     backlight.set_brightness(value.parse().unwrap());
 }
