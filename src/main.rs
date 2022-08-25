9d0438abd54c97bc2232126f0bf1a01295315f65
Brynley Llewellyn-Roux <49110391+brynblack@users.noreply.github.com>
2022-08-25 18:31:30 +1000

    Removed explicit backlight selection Program now automatically detects first backlight in the backlights directory and uses it

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use racklight::backlight::Backlight;
-use std::env;
+use std::{env, path::Path};
+
+const BACKLIGHT_PATH: &str = "/sys/class/backlight";
 
 fn main() {
     // Collect arguments
     let mut args = env::args();
-    let backlight = args.nth(1).unwrap();
-    let value = args.next().unwrap();
+    let value = args.nth(1).unwrap();
 
-    // Create a new backlight with the given path
-    let mut backlight = Backlight::new(backlight);
+    // Retrieve first backlight in directory
+    let mut backlights: Vec<_> = Path::new(BACKLIGHT_PATH)
+        .read_dir()
+        .unwrap()
+        .map(|d| d.unwrap())
+        .collect();
+
+    // Sort backlights alphabetically
+    backlights.sort_by_key(|d| d.path());
+
+    // Create a new backlight from the first backlight
+    let mut backlight = Backlight::new(backlights[0].path());
 
     // Set the backlight brightness to the given value
     backlight.set_brightness(value.parse().unwrap());
